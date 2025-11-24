a47628093585d25ea88a1a74076dc78419af03f5
David-Haley <david.haley@bigpond.com>
2025-11-24 18:16:27 +1000

    Day 21 part 2 solved, by counting the number of each subcode required to form the final code.

--- src/december_21.adb
+++ src/december_21.adb
@@ -9,8 +9,6 @@ with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Doubly_Linked_Lists;
 with Ada.Containers.Ordered_Maps;
 with Ada.Containers.Ordered_Sets;
-with Ada.Containers.Synchronized_Queue_Interfaces;
-with Ada.Containers.Unbounded_Synchronized_Queues;
 with DJH.Execution_Time; use DJH.Execution_Time;
 
 procedure December_21 is
@@ -24,18 +22,6 @@ procedure December_21 is
 
    subtype Door_Buttons is Character with
      Static_Predicate => Door_Buttons in 'A' | '0' .. '9';
-
-   -- Door Pad
-   -- X -2  -1   0    Y
-   --  +---+---+---+
-   --  | 7 | 8 | 9 | 3
-   --  +---+---+---+
-   --  | 4 | 5 | 6 | 2
-   --  +---+---+---+
-   --  | 1 | 2 | 3 | 1
-   --  +---+---+---+
-   --      | 0 | A | 0
-   --      +---+---+
 
    subtype Door_X is Ordinates range -2 .. 0;
    subtype Door_Y is Ordinates range 0 .. 3;
@@ -54,13 +40,6 @@ procedure December_21 is
 
    subtype Remote_Buttons is Character with
      Static_Predicate => Remote_Buttons in '^' | 'v' | '<' | '>' | 'A';
-
-   -- X -2  -1   0    Y
-   --      +---+---+
-   --      | ^ | A |  0
-   --  +---+---+---+
-   --  | < | v | > | -1
-   --  +---+---+---+
 
    subtype Remote_X is Ordinates range -2 .. 0;
    subtype Remote_Y is Ordinates range -1 .. 0;
@@ -82,21 +61,38 @@ procedure December_21 is
 
    subtype Codes is Unbounded_String;
 
-   End_Command : constant Codes := To_Unbounded_String ("End_Command");
-   Stop_Command : constant Codes := To_Unbounded_String ("Stop_Command");
+   Empty : constant Codes := Null_Unbounded_String;
 
    package Button_Maps is new
      Ada.Containers.Ordered_Maps (Button_Map_Keys, Codes);
    use Button_Maps;
 
+   subtype Big_Natural is Long_Long_Integer
+   range 0 .. Long_Long_Integer'Last;
+
+   package Frequency_Maps is new
+     Ada.Containers.Ordered_Maps (Codes, Big_Natural);
+   use Frequency_Maps;
+
    type Code_Elements is record
       Door_Code : Codes; -- The required door code
-      My_Code : Codes := Null_Unbounded_String; -- Code required to generate
+      My_Code : Codes := Empty; -- Code required to generate
+      Frequency_Map : Frequency_Maps.Map := Frequency_Maps.Empty_Map;
    end record; -- Code_Element
 
    package Code_Lists is new
      Ada.Containers.Doubly_Linked_Lists (Code_Elements);
    use Code_lists;
+
+   package Expanded_Codes is new Ada.Containers.Ordered_Sets (Codes);
+   use Expanded_Codes;
+
+   package Expansion_Tables is new
+     Ada.Containers.Ordered_Maps (Codes, Expanded_Codes.Set);
+   -- Initially the element type was Frequency_Maps.Map this allowed for more
+   -- than one instance of an expanded code. All possible codes did not produce
+   -- any repeats, so this was changed to a set.
+   use Expansion_Tables;
 
    procedure Read_Input (Code_List : out Code_Lists.List) is
 
@@ -126,7 +122,18 @@ procedure December_21 is
       function Door_Coordinate (Door_Button : in Door_Buttons)
                                 return Door_Coordinates is
 
-         -- Returns the absolute coordinates of the button
+         -- Returns the absolute coordinates of each door button.
+         -- Door Pad
+         -- X -2  -1   0    Y
+         --  +---+---+---+
+         --  | 7 | 8 | 9 | 3
+         --  +---+---+---+
+         --  | 4 | 5 | 6 | 2
+         --  +---+---+---+
+         --  | 1 | 2 | 3 | 1
+         --  +---+---+---+
+         --      | 0 | A | 0
+         --      +---+---+
 
       begin -- Door_Coordinate
          case Door_Button is
@@ -157,7 +164,7 @@ procedure December_21 is
 
       function Vertical (S, F : in Door_Buttons) return Codes is
 
-         Door_Code : Codes := Null_Unbounded_String;
+         Door_Code : Codes := Empty;
 
       begin -- Vertical
          if Door_Coordinate (S).Y < Door_Coordinate (F).Y then
@@ -177,7 +184,7 @@ procedure December_21 is
 
       function Horizontal (S, F : in Door_Buttons) return Codes is
 
-         Door_Code : Codes := Null_Unbounded_String;
+         Door_Code : Codes := Empty;
 
       begin -- Horizontal
          if Door_Coordinate (S).X < Door_Coordinate (F).X then
@@ -228,7 +235,14 @@ procedure December_21 is
       function Remote_Coordinate (Remote_Button : in Remote_Buttons)
                                   return Remote_Coordinates is
 
-         -- Returns the absolute coordinates of the robot remote control buttons
+         -- Returns the absolute coordinates of each robot remote control
+         -- button.
+         -- X -2  -1   0    Y
+         --      +---+---+
+         --      | ^ | A |  0
+         --  +---+---+---+
+         --  | < | v | > | -1
+         --  +---+---+---+
 
       begin -- Remote_Coordinate
          case Remote_Button is
@@ -247,7 +261,7 @@ procedure December_21 is
 
       function Vertical (S, F : in Remote_Buttons) return Codes is
 
-         Remote_Code : Codes := Null_Unbounded_String;
+         Remote_Code : Codes := Empty;
 
       begin -- Vertical
          if Remote_Coordinate (S).Y < Remote_Coordinate (F).Y then
@@ -267,7 +281,7 @@ procedure December_21 is
 
       function Horizontal (S, F : in Remote_Buttons) return Codes is
 
-         Remote_Code : Codes := Null_Unbounded_String;
+         Remote_Code : Codes := Empty;
 
       begin -- Horizontal
          if Remote_Coordinate (S).X < Remote_Coordinate (F).X then
@@ -319,7 +333,7 @@ procedure December_21 is
       Previous_Button : Door_Buttons := 'A';
 
    begin -- Door_Sequence
-      Code_Element.My_Code := Null_Unbounded_String;
+      Code_Element.My_Code := Empty;
       for D in Positive range 1 .. Length (Code_Element.Door_Code) loop
          Code_Element.My_Code := @ &
            Door_Map ((Previous_Button, Element (Code_Element.Door_Code, D)));
@@ -336,7 +350,7 @@ procedure December_21 is
       Door_Code : Unbounded_String := Code_Element.My_Code;
 
    begin -- Robot_Sequence
-      Code_Element.My_Code := Null_Unbounded_String;
+      Code_Element.My_Code := Empty;
       for R in Positive range 1 .. Length (Door_Code) loop
          Code_Element.My_Code := @ &
            Remote_Map ((Previous_Button, Element (Door_Code, R)));
@@ -358,9 +372,99 @@ procedure December_21 is
       return Code_Number * Sequence_Length;
    end Complexity;
 
+   procedure Build_Expansion_Table (Remote_Map : in Button_Maps.Map;
+                                    Expansion_Table : out Expansion_Tables.Map)
+   is
+
+      -- Builds a table of the codes reqired by robot N to produce a single
+      -- movement between keys for robot N+2.
+
+      Previous_Key : Remote_Buttons;
+
+   begin -- Build_Expansion_Table
+      Clear (Expansion_Table);
+      for R in Iterate (Remote_Map) loop
+         Insert (Expansion_Table, Element (R), Expanded_Codes.Empty_Set);
+      end loop; -- R in Iterate (Remote_Map)
+      for E in Iterate (Expansion_Table) loop
+         Previous_Key := 'A';
+         for C in Positive range 1 .. Length (Key (E)) loop
+            include (Expansion_Table (E),
+                     Remote_Map ((Previous_Key, Element (Key (E), C))));
+            Previous_Key := Element (Key (E), C);
+         end loop; -- C in Positive range 1 .. Length (Key (E))
+      end loop; -- E in Iterate (Expansion_Table)
+   end Build_Expansion_Table;
+
+   procedure Do_Robot (N_Robots : in Positive;
+                       Expasion_Table : in Expansion_Tables.Map;
+                       Code_Element : in out Code_Elements) is
+
+      Next_Robot : Frequency_Maps.Map := Frequency_Maps.Empty_Map;
+      First, Last : Positive := 1;
+      Previous_Key : Remote_Buttons := 'A';
+      Command : Codes;
+
+   begin -- Do_Robot
+      Clear (Code_Element.Frequency_Map);
+      while Last <= Length (Code_Element.My_Code) loop
+         while Last <= Length (Code_Element.My_Code) and then
+           Element (Code_Element.My_Code, Last) /= 'A' loop
+            Last := @ + 1;
+         end loop; -- Element (Code_Element.My_Code, Last) /= 'A'
+         if Last <= Length (Code_Element.My_Code) then
+            -- Command is a sequence ending in 'A' possibly just 'A'
+            Command := Unbounded_Slice (Code_Element.My_Code, First, Last);
+            if Contains (Code_Element.Frequency_Map, Command) then
+               Code_Element.Frequency_Map (Command) :=
+                 Code_Element.Frequency_Map (Command) + 1;
+            else
+               Insert (Code_Element.Frequency_Map, Command, 1);
+            end if; -- Contains (Code_Element.Frequency_Map,
+            Last := @ + 1;
+            First := Last;
+         end if; -- Last <= Length (Code_Element.My_Code)
+      end loop; -- Last <= Length (Code_Element.My_Code)
+      -- Code_Element.My_Code now containes the code for the robot operating
+      -- the first directional key pad. the remaining directional keypads are
+      -- managed below.
+      for R in Positive range 2 .. N_Robots loop
+         Clear (Next_Robot);
+         for C in Iterate (Code_Element.Frequency_Map) loop
+            for E in Iterate (Expasion_Table (Key (C))) loop
+               if not Contains (Next_Robot, Element (E)) then
+                  Insert (Next_Robot, Element (E), 0);
+               end if; -- not Contains (Next_Robot, Element (E))
+               Next_Robot (Element (E)) :=
+                 Next_Robot (Element (E)) + Element (C);
+            end loop; -- E in Iterate (Expasion_Table (Key (C)))
+         end loop; -- C in Iterate (Code_Element.Frequency_Map)
+         Code_Element.Frequency_Map := Copy (Next_Robot);
+      end loop; -- R in Positive range 2 .. N_Robots
+   end Do_Robot;
+
+   function Complexity_2 (Code_Element : in Code_Elements) return Big_Natural is
+
+      First : Positive;
+      Last : Natural;
+      Code_Number, Total_Length : Big_Natural := 0;
+
+   begin -- Complexity_2
+      Find_Token (Code_Element.Door_Code, Decimal_Digit_Set, Inside, First,
+                  Last);
+      Code_Number :=
+        Big_Natural'Value (Slice (Code_Element.Door_Code, First, Last));
+      for C in Iterate (Code_Element.Frequency_Map) loop
+         Total_Length := @ + Big_Natural (Length(Key (C))) * Element (C);
+      end loop; -- C in Iterate (Code_Element.Frequency_Map)
+      return Code_Number * Total_Length;
+   end Complexity_2;
+
    Code_List : Code_Lists.List;
    Door_Map, Remote_Map : Button_Maps.Map;
    Sum : Natural := 0;
+   Expansion_Table : Expansion_Tables.Map;
+   Sum_2 : Big_Natural := 0;
 
 begin -- December_21
    Read_Input (Code_List);
@@ -374,6 +478,15 @@ begin -- December_21
    end loop; -- C in Iterate (Code_List)
    Put_Line ("Part one:" & Sum'Img);
    DJH.Execution_Time.Put_CPU_Time;
-   Put_Line ("Part two:");
+   Build_Expansion_Table (Remote_Map, Expansion_Table);
+   for C in Iterate (Code_List) loop
+      Code_List (C).My_Code := Empty;
+      Door_Sequence (Door_Map, Code_List (C));
+      Robot_Sequence (Remote_Map, Code_List (C));
+      -- Code list contains code for first robot to robot sequences.
+      Do_Robot (25, Expansion_Table, Code_List (C));
+      Sum_2 := @ + Complexity_2 (Element (C));
+   end loop; -- C in Iterate (Code_List)
+   Put_Line ("Part two:" & Sum_2'Img);
    DJH.Execution_Time.Put_CPU_Time;
 end December_21;
